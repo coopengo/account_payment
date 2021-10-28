1267b5004a53e665790164a0ef2a7270fcfc9970
Nicolas Evrard <nicoe@openhex.org>
2021-10-28 11:20:08 +0200

    Add missing Decimal import

--- payment.py
+++ payment.py
@@ -1,5 +1,6 @@
 # This file is part of Tryton.  The COPYRIGHT file at the top level of
 # this repository contains the full copyright notices and license terms.
+from decimal import Decimal
 from itertools import groupby
 
 from trytond.i18n import gettext
